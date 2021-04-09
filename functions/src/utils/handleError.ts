19e7c942542d798dabf748e526b9107a0bd2ef36
Mahedi Hasan <mahedi.hs1997@gmail.com>
2021-04-09 16:08:48 +0100

    mini changes

--- functions/src/utils/handleError.ts
+++ functions/src/utils/handleError.ts
@@ -2,12 +2,12 @@ import { StructError } from 'superstruct';
 
 export const handleError = (err: any): { code: number; message: string } => {
   if (err instanceof StructError) {
-    return { code: 401, message: err.message };
+    return { code: 400, message: err.message };
   }
 
   if (typeof err === 'object' && err !== null) {
-    return { code: 401, message: err.message };
+    return { code: 500, message: err.message };
   }
 
-  return { code: 500, message: 'Internal Server Error' };
+  return { code: 500, message: 'Something went wrong and I dont know what' };
 };
